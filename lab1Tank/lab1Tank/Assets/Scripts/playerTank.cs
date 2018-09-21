9b4c16260cd8901ab1212a5ac8aa59f561a0569a
david-p-burton <david.p.burton@gmail.com>
2018-09-21 17:10:46 +0100

    Half way there!

---
+++ lab1Tank/lab1Tank/Assets/Scripts/playerTank.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerTank : MonoBehaviour {
+
+	public float movementSpeed;
+	public float rotationalSpeed;
+//
+//	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+//		ROLLING BALL MOVEMENT
+//		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		
+		var horz = Input.GetAxis("Horizontal") * Time.deltaTime * rotationalSpeed;
+		var vert = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+		transform.Rotate(0, horz, 0);
+		transform.Translate(0, 0, vert);
+
+//		ROLLING BALL MOVEMENT
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+//		float moveVertical = Input.GetAxis ("Vertical");
+//
+//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+//
+//		rb.AddForce (movement * speed * 10);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ lab1Tank/lab1Tank/Assets/colorChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChanger : MonoBehaviour {
+
+	public Renderer rend;
+	public Shader shader;
+	public Texture texture;
+	public Color color;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+
+		Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f );
+		rend.material = new Material (shader);
+		rend.material.mainTexture = texture;
+		rend.material.color = newColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f );
+		rend.material.color = newColor;
+	}
+}
