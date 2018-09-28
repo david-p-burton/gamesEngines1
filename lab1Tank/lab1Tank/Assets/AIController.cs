7cb7f7da06915839ff9ff86611bc738d63b13758
david-p-burton <david.p.burton@gmail.com>
2018-09-28 16:41:53 +0100

    Look at my circle of blocks

---
+++ lab1Tank/lab1Tank/Assets/AIController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour {
+
+	public float movementSpeed;
+	public float rotationalSpeed;
+	public int wayPoints;
+	public Transform[] points;
+	private int destPoint;
+	public float radius;
+
+	// Use this for initialization
+	void Start () {
+		wayPointGenerator ();
+		goToNextPoint ();
+	}
+
+	//make waypoints
+	void wayPointGenerator ()	{
+		float theta = 360 / wayPoints;
+		print (theta);
+
+
+		for (int i = 0; i < wayPoints; i++) {
+//			GameObject newPoint = new GameObject();
+			GameObject newPoint = Resources.Load ("Target") as GameObject;
+
+			GameObject use = Instantiate (newPoint);
+
+			use.transform.position = new Vector3 (Mathf.Sin(theta * i * Mathf.Deg2Rad) * radius, 0.5f, Mathf.Cos(theta * i  * Mathf.Deg2Rad) * radius);
+			print ( Mathf.Cos(theta * i) + radius  +  "  " + Mathf.Sin(theta * i) + radius);
+			//gameObject.transform.Translate(1, 1, 1);
+		}
+	}
+
+	void goToNextPoint() {
+		destPoint++;
+		if (destPoint > wayPoints) {
+			destPoint = 1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ lab1Tank/lab1Tank/Assets/Sway.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sway : MonoBehaviour {
+
+
+	public float angle = 20.0f;
+	public float freq = 0.5f;
+	//should be private
+	public float theta;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//local rotation is rotation relative to its parent rather than the world
+		transform.localRotation = Quaternion.AngleAxis (
+			Mathf.Sin (theta) * angle, Vector3.forward);
+		theta += freq * Time.deltaTime * Mathf.PI * 2.0f;
+
+
+	}
+}
