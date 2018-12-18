f17977bd1db8c61854d6a6b04e79938014182a51
david-p-burton <david.p.burton@gmail.com>
2018-12-18 21:50:36 +0000

    Added more particle generators. Trying out noise filters on particles now

---
+++ assignment/GEAssignment/Assets/cameraRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRotation : MonoBehaviour {
+
+    public GameObject focus;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(focus.transform);
+		
+	}
+}
